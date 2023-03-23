2534da176e9b208e37b252428f30355c2cc759d1
sakethkhandavalli <sakethkhandavalli@gmail.com>
2023-03-22 20:55:22 -0700

    Update testing params

--- dataset_generation.py
+++ dataset_generation.py
@@ -3,11 +3,11 @@ import random
 
 def generate_random_MIP(num_vars, num_constraints):
     # random objective coefficients
-    c = np.random.randint(0, 50, num_vars)
+    c = np.random.randint(0, 500, num_vars)
 
     # generate random coefficients for the constraints
-    A = np.random.randint(0, 25, size=(num_constraints, num_vars))
-    b = np.random.randint(0, 40, num_constraints)
+    A = np.random.randint(0, 250, size=(num_constraints, num_vars))
+    b = np.random.randint(0, 400, num_constraints)
 
     # fixed bounds
     bounds = [(0, None)] * num_vars
@@ -22,7 +22,7 @@ def generate_random_MIP(num_vars, num_constraints):
 def generate_dataset(no_problems):
     problems = []
     for i in range(no_problems):
-        num_vars = random.randint(10, 3000)
-        num_constraints = random.randint(10, 3000)
+        num_vars = random.randint(2000, 5000)
+        num_constraints = random.randint(1000, 5000)
         problems.append(generate_random_MIP(num_vars, num_constraints))
     return problems
